1faec23b262b6992eeaa02d47ae5ee4b7fbcbf18
Rupesh Mangalam <91172425+RupeshMangalam21@users.noreply.github.com>
2023-03-23 16:58:53 +0000

    Added Header.jsx and Footer.jsx in MainComponents

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navbar">
+        <div className="nav-title">AVA-H</div>
+      </nav>
+        </div>
+    );
+}
+
+export default Header;
