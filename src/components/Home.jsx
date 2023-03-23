7a238f7c19892bd794c9062462bc507d264f0bda
Rupesh Mangalam <91172425+RupeshMangalam21@users.noreply.github.com>
2023-03-23 16:44:09 +0000

    UI changes for Home.jsx Header.jsx and Footer.jsx components added in MainComponent.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,52 +1,52 @@
 import React from 'react';
 import '../style/Home.css';
 import Card from 'react-bootstrap/Card';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <div className="card-container">
-        <div >
-          <Card className="card" style={{ width: '18rem', height: '15rem' }}>
+  return (
+    <div>
+            <div className="card-container">
+        <div>
+          <Card className="card" style={{ width: '25rem', height: '16rem' }}>
             <Card.Body>
               <Card.Title>Patient</Card.Title>
               <Card.Text>
                 Join a supportive community of patients and healthcare providers.
                 Sign up today and gain access to a wealth of health resources and information.
               </Card.Text>
-              <Link to={"/LogIn"}> <Card.Link>Login now</Card.Link></Link>
-             
+              <Link to={"/LogIn"} className="login-btn"> <Card.Link>Login</Card.Link></Link>
+
             </Card.Body>
           </Card>
         </div>
         <div>
-          <Card className="card" style={{ width: '18rem', height: '15rem'}}>
+          <Card className="card" style={{ width: '25rem', height: '16rem' }}>
             <Card.Body>
               <Card.Title>Doctor</Card.Title>
               <Card.Text>
                 Access your secure account to view patient information and update records.
                 Join a supportive community of patients and healthcare providers.
               </Card.Text>
-              <Link to={"/LogIn"}> <Card.Link>Login now</Card.Link></Link>
+              <Link to={"/LogIn"} className="login-btn"> <Card.Link>Login</Card.Link></Link>
             </Card.Body>
           </Card>
         </div>
-        <div >
-          <Card className="card" style={{ width: '18rem', height: '15rem' }}>
+        <div>
+          <Card className="card" style={{ width: '25rem', height: '16rem' }}>
             <Card.Body>
               <Card.Title>DBA</Card.Title>
               <Card.Text>
                 Manage your databases with ease and efficiency.
                 Join a supportive community of patients and healthcare providers.
               </Card.Text>
-               <Link to={"/LogIn"} className="login-btn"> <Card.Link>Login now</Card.Link></Link>
-            
+              <Link to={"/LogIn"} className="login-btn"> <Card.Link>Login</Card.Link></Link>
             </Card.Body>
           </Card>
         </div>
       </div>
-      
-    );
+    </div>
+  );
 }
 
 export default Home;
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LogIn from './auth/LogIn'
 import SignUp from './auth/SignUp'
+import Header from './Header'
 import Home from './Home'
+import Footer from './Footer'
 export default function MainComponent() {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/LogIn' element={<LogIn/>}/>
         <Route path='/SignUp' element={<SignUp/>}/>
       </Routes>
-    
+    <Footer/>
     
     </div>
   )
