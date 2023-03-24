a1523ee2fd03521c9cda3a39bc4ac555b8b753e0
Rupesh Mangalam <91172425+RupeshMangalam21@users.noreply.github.com>
2023-03-24 17:50:30 +0000

    UI-changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,22 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <div style={{ minHeight: '100vh', position: 'relative' }}>
-            <footer style={{ backgroundColor: '#757376', padding: '10px', textAlign: 'center', position: 'absolute', bottom: '0', width: '100%'}}>
-        <p style={{ fontSize: '14px', color: '#fff' }}>Copyright © 2023 AVA-H.
+            <div style={{ minHeight: '27vh', position: 'relative' }}>
+            <footer style={{ 
+  background: 'linear-gradient(to right, #9254d8, #99dbe5)',
+  padding: '10px',
+  textAlign: 'center',
+  position: 'absolute',
+  bottom: '0',
+  width: '100%'
+}}>
+        <p style={{ fontSize: '14px', color: 'white' }}>Copyright © 2023 AVA-H.
           All rights reserved.</p>
       </footer>
     </div>
         </div>
     );
 }
+
 
 export default Footer;
