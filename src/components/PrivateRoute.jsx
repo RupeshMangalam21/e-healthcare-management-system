23a740cea0fb30f1d9391e80f3b749fc26f8cb99
Prince panwar <99018927+prince-panwar@users.noreply.github.com>
2023-03-25 18:36:13 +0000

    Private Routes setup signup button fixed

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,21 +1,10 @@
 import React,{useContext}from 'react';
-import { Route,Navigate } from 'react-router-dom';
+import { Outlet,Navigate } from 'react-router-dom';
 import { AuthContext } from './auth/AuthProvider';
 
-const PrivateRoute = ({component: RouteComponent, ...rest}) => {
-    const {CurrentUser}=useContext(AuthContext);
-    return (
-        <Route
-        {...rest}
-        render={routeProps =>
-          !!CurrentUser ? (
-            <RouteComponent {...routeProps} />
-          ) : (
-            <Navigate to={"/LogIn"} />
-          )
-        }
-      />
-    );
-}      
-
-export default PrivateRoute;
+export default function PrivateRoute() {
+  const {CurrentUser}=useContext(AuthContext);
+  return (
+   CurrentUser?<Outlet/>: <Navigate to="/LogIn"/>
+  );
+}
