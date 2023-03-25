91198a87097f5ae41dfef579a69edf08c69cb733
Rupesh Mangalam <91172425+RupeshMangalam21@users.noreply.github.com>
2023-03-25 22:18:18 +0000

    added-bg-login

--- src/components/auth/LogIn.jsx
+++ src/components/auth/LogIn.jsx
@@ -1,58 +1,59 @@
-import React, { useState,useContext } from 'react'
+import React, { useState, useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import '../../style/LogIn.css';
-import{auth} from '../../lib/init-firebase';
+import { auth } from '../../lib/init-firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import {Link,Navigate} from 'react-router-dom';
-import {AuthContext} from './AuthProvider';
-
+import { Link, Navigate } from 'react-router-dom';
+import { AuthContext } from './AuthProvider';
 
 export default function LogIn() {
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    function handleSubmit(e){
-        e.preventDefault();
-        signInWithEmailAndPassword(auth,email,password).then((useCredentials) =>{
-            console.log(useCredentials);
-        }).catch((error) =>{
-            console.log(error);
-        })
-      
-    }
-    const {CurrentUser} = useContext(AuthContext)
-    if(CurrentUser){
-      return <Navigate to={"/DashBoard"}/>
-    }
-    return (
-    
-    <div>
-        <div className="form-container">
-       <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" onChange={(e)=> setEmail(e.target.value)} />
-       
-      </Form.Group>
+  function handleSubmit(e) {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((useCredentials) => {
+        console.log(useCredentials);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" onChange={(e)=> setPassword(e.target.value)} />
-      </Form.Group>
-      <div className="log-btn">
-         <Button className="login-btn"  type="submit">
-        login
-      </Button>
+  const { CurrentUser } = useContext(AuthContext);
+  if (CurrentUser) {
+    return <Navigate to={'/DashBoard'} />;
+  }
+
+  return (
+    <div className="login-page">
+      <div className="form-container">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          </Form.Group>
+
+          <div className="log-btn">
+            <Button className="login-btn" type="submit">
+              login
+            </Button>
+          </div>
+
+          <div className="sign-btn">
+            <Link to={'/SignUp'} className="signup-btn">
+              Sign Up
+            </Link>
+          </div>
+        </Form>
       </div>
-      <div className="sign-btn">
-  <Link to={"/SignUp"} className="signup-btn">
-    Sign Up
-  </Link>
-</div>
-   
-    </Form>
     </div>
-    </div>
-  )
+  );
 }
