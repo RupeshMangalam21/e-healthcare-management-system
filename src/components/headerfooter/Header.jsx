bc832672668a522dd2f67db83ba5a00994c29ac6
Prince panwar <99018927+prince-panwar@users.noreply.github.com>
2023-04-11 18:07:08 +0000

    Changed sidebar design and Added sidebar button in header

--- src/components/headerfooter/Header.jsx
+++ src/components/headerfooter/Header.jsx
@@ -1,10 +1,14 @@
 import React, { useContext } from 'react';
 import { FaSignOutAlt } from 'react-icons/fa';
+
 import logo from '../../assets/logo-img/logo_avah.png';
 import '../../style/Headerfooter.css';
+
 import { auth } from '../../lib/init-firebase';
 import { AuthContext } from '../auth/AuthProvider';
 import { signOut } from 'firebase/auth';
+
+import SideBar from '../SideBar';
 function Header() {
   const { CurrentUser} = useContext(AuthContext);
 
@@ -17,7 +21,13 @@ function Header() {
 
   return (
     <div className="header">
+   
       <nav className="navbar">
+      {CurrentUser && (
+          <div className="nav-sign-out">
+          <SideBar/>
+          </div>
+        )}
         <div className="nav-logo">
           <img src={logo} alt="AVA-H Logo" style={{ width: '67px', height: '67px', placeItems: 'center' }} />
           <div className="nav-title">AVA-H</div>
