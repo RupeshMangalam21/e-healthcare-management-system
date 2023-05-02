3e4d5214acf8a39eecd64528c2e7283d31bb494c
Rupesh Mangalam <91172425+RupeshMangalam21@users.noreply.github.com>
2023-05-02 20:12:01 +0000

    revert

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,16 +1,13 @@
 function Dashboard() {
+
+
   return (
     <div>
-      <h1>Welcome to your dashboard!</h1>
-      <p>Here is some dummy content to get you started:</p>
-      <ul>
-        <li>Task 1</li>
-        <li>Task 2</li>
-        <li>Task 3</li>
-      </ul>
-      <p>Feel free to customize this dashboard as you see fit!</p>
+      
     </div>
   );
 }
 
 export default Dashboard;
+
+
