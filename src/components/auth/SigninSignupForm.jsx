92228650859bcb0116c42231c14acc7de2fa1cbe
RupeshMangalam22 <133216270+RupeshMangalam22@users.noreply.github.com>
2023-05-13 12:22:17 +0000

    build-fixes + temporary changes

--- src/components/auth/SigninSignupForm.jsx
+++ src/components/auth/SigninSignupForm.jsx
@@ -1,60 +1,60 @@
 import React from 'react';
 import "../../style/SigninSignupForm.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faEnvelope, faLock } from '@fortawesome/react-fontawesome'
+import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons'
 
 const SigninSignupForm = () => {
   return (
-    <div className="container">
-      <div className="forms-container">
-        <div className="signin-signup">
-          <form action="#" className="sign-in-form">
-            <h2 className="title">Sign in</h2>
-            <div className="input-field">
+    <div className="container-ss">
+      <div className="forms-container-ss">
+        <div className="signin-signup-ss">
+          <form action="#" className="sign-in-form-ss">
+            <h2 className="title-ss">Sign in</h2>
+            <div className="input-field-ss">
             <FontAwesomeIcon icon={faUser} />
             <input type="text" placeholder="Username" />
             </div>
-            <div className="input-field">
+            <div className="input-field-ss">
             <FontAwesomeIcon icon={faLock} />
             <input type="password" placeholder="Password" />
             </div>
-            <input type="submit" value="Login" className="btn solid" />
+            <input type="submit" value="Login" className="btn-ss solid" />
           </form>
-          <form action="#" className="sign-up-form">
-            <h2 className="title">Sign up</h2>
-            <div className="input-field">
+          <form action="#" className="sign-up-form-ss">
+            <h2 className="title-ss">Sign up</h2>
+            <div className="input-field-ss">
             <FontAwesomeIcon icon={faUser} />
             <input type="text" placeholder="Username" />
             </div>
-            <div className="input-field">
+            <div className="input-field-ss">
             <FontAwesomeIcon icon={faLock} />
             <input type="password" placeholder="Password" />
             </div>
-            <div className="input-field">
+            <div className="input-field-ss">
             <FontAwesomeIcon icon={faEnvelope} />
             <input type="email" placeholder="Email" />
             </div>
-            <input type="submit" className="btn" value="Sign up" />
+            <input type="submit" className="btn-ss" value="Sign up" />
           </form>
         </div>
       </div>
 
-      <div className="panels-container">
-        <div className="panel left-panel">
-          <div className="content">
+      <div className="panels-container-ss">
+        <div className="panel-ss left-panel-ss">
+          <div className="content-ss">
             <h3>New here ?</h3>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
               ex ratione. Aliquid!
             </p>
-            <button className="btn transparent" id="sign-up-btn">
+            <button className="btn-ss transparent" id="sign-up-btn">
               Sign up
             </button>
           </div>
           <img src="src/assets/log.svg" className="image" alt="" />
         </div>
-        <div className="panel right-panel">
-          <div className="content">
+        <div className="panel-ss right-panel-ss">
+          <div className="content-ss">
             <h3>One of us ?</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
@@ -64,7 +64,7 @@ const SigninSignupForm = () => {
               Sign in
             </button>
           </div>
-          <img src="src/assets/register.svg" className="image" alt="" />
+          <img src="src/assets/register.svg" className="image-ss" alt="" />
         </div>
       </div>
       <script src="index.html"></script>
