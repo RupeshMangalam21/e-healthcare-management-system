9df649d093cc412c5fff8e5e92881c1bb57b44c4
Prince panwar <99018927+prince-panwar@users.noreply.github.com>
2023-05-17 06:59:55 +0000

    Medical Data updated

--- src/components/dashboard/AddMedicalHistory.jsx
+++ src/components/dashboard/AddMedicalHistory.jsx
@@ -7,21 +7,24 @@ import {addDoc,collection, query, where, getDocs,} from 'firebase/firestore';
 const AddMedicalHistory = ({patient}) => {
    
     const [patientName, setPatientName] = useState("");
+    const[patientAge,setPatientAge]=useState("");
+    const[patientGender,setPatientGender]=useState("");
+    const[patientBlood,setPatientBlood]=useState("");
     const [diagnosis, setDiagnosis] = useState("");
     const [docId, setDocId] = useState("");
-   const [doctorName, setDoctorName] = useState("");
-   
+    const [doctorName, setDoctorName] = useState("");
     const [prescription, setPrescription] = useState("");
     const [remark, setRemark] = useState("");
 
-    
-   
     const handleSubmit = (e) => {
         e.preventDefault();
     const historyRef =collection(firestore,'MedicalHistory')
     addDoc(historyRef,{
         Date: serverTimestamp(),
         patientName: patientName,
+        bloodGroup: patientBlood,
+        age:patientAge,
+        gender: patientGender,
         doctorName: doctorName,
         docId: docId,
         patientId: patient,
@@ -31,8 +34,6 @@ const AddMedicalHistory = ({patient}) => {
     }).catch((error)=>{
         console.log(error);
     })
-       
-       
         setPatientName("");
         setDiagnosis("");
         setDocId("");
@@ -70,6 +71,10 @@ const AddMedicalHistory = ({patient}) => {
             const patientDoc = querySnapshot.docs[0];
             const patientData = patientDoc.data();
             setPatientName(patientData.name);
+            setPatientGender(patientData.gender);
+            setPatientAge(patientData.age);
+            setPatientBlood(patientData.bloodGroup);
+
             
           }
         }).catch((error) => {
@@ -82,51 +87,57 @@ const AddMedicalHistory = ({patient}) => {
         <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formDate">
             
-    
-          <Form.Group className="mb-3" controlId="formDiagnosis">
+       <Form.Group className="mb-3" controlId="formDiagnosis">
             <Form.Label>Patient Name</Form.Label>
             <Form.Control type="text" defaultValue={patientName} />
           </Form.Group>
-    
-          <Form.Group className="mb-3" controlId="formDiagnosis">
-            <Form.Label>Doctor Name</Form.Label>
-            <Form.Control type="text" defaultValue={doctorName}  />
-          </Form.Group>
-         
-       
-         
-       
           </Form.Group>
          
           <Form.Group className="mb-3" controlId="formDiagnosis">
             <Form.Label>Patient ID</Form.Label>
             <Form.Control type="text" value={patient} />
           </Form.Group>
+         
           <Form.Group className="mb-3" controlId="formDiagnosis">
+            <Form.Label>Patient age</Form.Label>
+            <Form.Control type="number" value={patientAge} />
+          </Form.Group>
+         
+          <Form.Group className="mb-3" controlId="formDiagnosis">
+            <Form.Label>Patient gender</Form.Label>
+            <Form.Control type="text" value={patientGender} />
+          </Form.Group>
+         
+          <Form.Group className="mb-3" controlId="formDiagnosis">
+            <Form.Label>Patient Blood-Group</Form.Label>
+            <Form.Control type="number" value={patientBlood} />
+          </Form.Group>
+
+         <Form.Group className="mb-3" controlId="formDiagnosis">
+            <Form.Label>Doctor Name</Form.Label>
+            <Form.Control type="text" defaultValue={doctorName}  />
+          </Form.Group>
+      
+       <Form.Group className="mb-3" controlId="formDiagnosis">
             <Form.Label>Doctor ID</Form.Label>
             <Form.Control type="text" value={docId} />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formDiagnosis">
+     <Form.Group className="mb-3" controlId="formDiagnosis">
             <Form.Label>Diagnosis</Form.Label>
             <Form.Control type="text" value={diagnosis} onChange={(e) => setDiagnosis(e.target.value)} />
           </Form.Group>
           
-          <Form.Group className="mb-3" controlId="formDiagnosis">
+     <Form.Group className="mb-3" controlId="formDiagnosis">
             <Form.Label>prescription</Form.Label>
             <Form.Control type="text" value={prescription} onChange={(e) => setPrescription(e.target.value)} />
           </Form.Group>
          
-          <Form.Group className="mb-3" controlId="formDiagnosis">
+    <Form.Group className="mb-3" controlId="formDiagnosis">
             <Form.Label>Remark</Form.Label>
             <Form.Control type="text" value={remark} onChange={(e) => setRemark(e.target.value)} />
           </Form.Group>
-         
-         
-    
-    
-    
-          <Button type="submit">Submit</Button>
+    <Button type="submit">Submit</Button>
         </Form>
       );
     };
